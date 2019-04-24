658e61620fc50718ab35f1508330b13e94fa6ec6
b5027345 <b5027345@my.shu.ac.uk>
2019-04-24 16:35:31 +0100

    Created reviewApp folder whre applicatin will be built

--- reviewApp/config/routes.rb
+++ reviewApp/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root 'static_pages#home'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  resources :products
+  get 'static_pages/product'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
